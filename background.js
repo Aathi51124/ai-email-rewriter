48b35b1571d230e54618d2a0990e5a4dc9b891c7
Aathi51124 <727722euai001@skcet.ac.in>
2025-07-03 10:39:27 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,4 +1,3 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log("AI Email Rewriter installed.");
 });
-// Nothing needed for now. This file is reserved if we want to inject anything automatically later.
